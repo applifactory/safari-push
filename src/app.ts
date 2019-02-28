016223bfcdd87115916056c067579bcecfde0ea8
Mateusz Witaliński <matthew.w@314t.com>
2019-02-28 10:07:34 +0100

    Add ssl

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express = require('express');
+import bodyParser = require('body-parser');
+import https = require('https');
+import fs = require('fs');
+import path = require('path');
 
 const app: express.Application = express();
 
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+// Render main site
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(`
+    <html><head><title>Safari Push Example</title></head><body><h1>Safari Push Example</h1><div id="log"></div><script src="/js/index.js" type="text/javascript"></script></body></html>
+  `);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000');
-});
+// Serve via https
+const httpsOptions = {
+  key: fs.readFileSync('./private/ssl/key.pem'),
+  cert: fs.readFileSync('./private/ssl/cert.pem')
+}
+const server = https.createServer(httpsOptions, app).listen(3000, () => {
+  console.log('Example app listening on port https://HOSTNAME:3000');
+})
