0a0a21a1c2ed8692e74af15768d29c4dbc866706
Mateusz Witaliński <matthew.w@314t.com>
2019-03-18 22:48:30 +0100

    Add simple notification send method with node-apn

--- public/js/index.js
+++ public/js/index.js
@@ -17,10 +17,32 @@ var checkRemotePermission = function (permissionData) {
   else if (permissionData.permission === 'granted') {
     // The web service URL is a valid push provider, and the user said yes.
     // permissionData.deviceToken is now available to use.
-    console.log('deviceToken', permissionData.deviceToken);
+    document.getElementById('log').innerHTML += '<br>permission granted, sending push in 1 second';
+    setTimeout(function() {
+      // Dispatch message sending after one second, so you can hide the browser
+      fetch('/push', 
+        {
+          method: 'post',
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({
+            "title": "ROAR!",
+            "message": "Message from the Lion",
+            "token": permissionData.deviceToken
+          })
+        }
+      ).then(function (response) {
+        response.json().then( function(result) { 
+          document.getElementById('log').innerHTML += `<br>${ response.status == 200 ? 'success' : 'error' }: ${result.message}`;
+        });
+      });
+    }, 1000);
+    
   }
 };
 
+// Initialize only in Safari, only on desktop (iOS `pushNotification` doesn't exist)
 if ('safari' in window && 'pushNotification' in window.safari) {
   var permissionData = window.safari.pushNotification.permission('web.com.leocode.login');
   checkRemotePermission(permissionData);
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser = require('body-parser');
 import https = require('https');
 import fs = require('fs');
 import path = require('path');
+import apn = require('apn');
 
 const app: express.Application = express();
 
@@ -21,11 +22,11 @@ app.get('/v(\\d)/pushPackages/web.com.leocode.login', function (req, res) {
   // Search for the latest zipped pushPackage
   let pushPackage = fs.readdirSync(path.resolve(__dirname, '../private')).reverse().find( (name) => !!name.match(/^.*\.zip$/) );
   if ( pushPackage ) {
-    // Send back the package
+    // Send the package file
     res.set('Content-type', 'application/zip');
     res.sendFile( path.resolve(__dirname, `../private/${pushPackage}`) );
   } else {
-    // Sorry, no package found
+    // Sorry, no package file found
     res.status(404);
     res.end();
   }
@@ -36,6 +37,57 @@ app.post('/v(\\d)/log', function (req, res) {
   console.log('log: ', req.body.logs);
   res.end();
 });
+
+app.post('/push', function(req, res){
+
+  console.log('push', req.body);
+  const { title, message, token } = req.body;
+  if ( !title || !message || !token ) {
+    res
+      .status(400)
+      .json({
+        message: 'Parameter is missing'
+      })
+      .end();
+  }
+
+  // Create push provider
+  var options = {
+    gateway: "gateway.sandbox.push.apple.com",
+    cert: path.resolve(__dirname, '../private/aps.cert/web_aps_prod_cert.pem'),
+    key:  path.resolve(__dirname, '../private/aps.cert/web_aps_prod_key.pem'),
+    production: true
+  };
+  var apnProvider = new apn.Provider(options);
+
+
+  // Create note with payload
+  const note = new apn.Notification();
+  note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+  note.urlArgs = ["my_url_argument"];
+  note.alert = {
+    title,
+    body: message
+  };
+  note.topic = "web.com.leocode.login"; // my app bundle
+  
+  // Send notification
+  apnProvider.send(note, token).then( (result) => {
+    if ( result.failed && result.failed.length ) {
+      res.json({
+        message: 'Message send failed',
+        reason: result.failed.pop().response.reason
+      });
+    } else {
+      res.json({
+        message: "Message sent!"
+      });
+    }
+    res.end();
+  });
+
+})
+
 
 // Serve via https
 const httpsOptions = {
